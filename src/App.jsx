389a79657060d299ce0f1e3da2072f069f020768
Jass <136769749+frontendjass@users.noreply.github.com>
2024-09-03 22:03:02 -0500

    Implement simple polaroid like Photo component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import { motion } from 'framer-motion';
+import Picture from './components/Picture';
 
 function App() {
-  const readyText = 'Ready to go!';
-  const rainbowText = [
-    '#ff0000',
-    '#ff7f00',
-    '#ffff00',
-    '#00ff00',
-    '#0000ff',
-    '#4b0082',
-    '#9400d3',
-  ];
-
   return (
-    <div className='h-screen flex flex-col justify-center items-center'>
-      <h1>
-        {readyText.split('').map((letter, index) => (
-          <motion.span
-            key={index}
-            initial={{ color: '#FFFFFF' }}
-            animate={{ color: rainbowText }}
-            transition={{ duration: 2, delay: index * 0.1, repeat: Infinity }}
-            className='text-5xl'
-          >
-            {letter}
-          </motion.span>
-        ))}
-      </h1>
-      <p className='text-center'>If the above text is centered and the color changes, it indicates that Tailwind and Framer Motion have been installed correctly!</p>
-    </div>
+    <main className='bg-[#1e1e1e]'>
+      <div className='grid justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+        <div>
+          <Picture />
+          <Picture />
+          <Picture />
+        </div>
+        <div>
+          <Picture />
+          <Picture />
+          <Picture />
+        </div>
+        <div>
+          <Picture />
+          <Picture />
+          <Picture />
+        </div>
+        <div>
+          <Picture />
+          <Picture />
+          <Picture />
+        </div>
+        <div>
+          <Picture />
+          <Picture />
+          <Picture />
+        </div>
+      </div>
+    </main>
   );
 }
 
